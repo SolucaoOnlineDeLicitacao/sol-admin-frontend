e99b9f48a0fab2f7839a577759f1a1b21a5d17b5
David Pedoneze <dpedoneze@caiena.net>
2019-10-18 12:47:46 -0300

    Alterar máscara de coordenadas para aceitar um dígito antes do divisor #2

--- src/views/addresses/address-fields.vue
+++ src/views/addresses/address-fields.vue
@@ -60,7 +60,7 @@
           :name="prefix + '[address_attributes][latitude]'",
           :error="errors[errorPrefix + 'address.latitude']",
           placeholder="00.00000000",
-          :mask="{ mask: 'M00.00000000' }"
+          :mask="{ mask: 'M99.00000000' }"
         )
 
       .three.columns
@@ -71,7 +71,7 @@
           :name="prefix + '[address_attributes][longitude]'",
           :error="errors[errorPrefix + 'address.longitude']",
           placeholder="00.00000000",
-          :mask="{ mask: 'M00.00000000' }"
+          :mask="{ mask: 'M99.00000000' }"
         )
 
     .row
